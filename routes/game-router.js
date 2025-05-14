818390e3754cd931452db73aeaee65769280469b
unknown <iamevannt@gmail.com>
2025-05-14 12:21:11 -0400

    Implement Game Logic
    Removes testing information

--- routes/game-router.js
+++ routes/game-router.js
@@ -2,20 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { Game } = require("../models/game");
 
-// const testHand = [
-//     // new Card(1, "2", "DIAMONDS", "https://deckofcardsapi.com/static/img/2D.png"),
-//     // new Card(2, "3", "DIAMONDS", "https://deckofcardsapi.com/static/img/3D.png"),
-//     // new Card(3, "4", "DIAMONDS", "https://deckofcardsapi.com/static/img/4D.png"),
-//     // new Card(4, "5", "DIAMONDS", "https://deckofcardsapi.com/static/img/5D.png"),
-//     // new Card(5, "6", "SPADES", "https://deckofcardsapi.com/static/img/6S.png"),
-//     new Card(6, "10", "SPADES", "https://deckofcardsapi.com/static/img/0S.png"),
-//     new Card(7, "9", "DIAMONDS", "https://deckofcardsapi.com/static/img/9D.png"),
-//     new Card(8, "10", "DIAMONDS", "https://deckofcardsapi.com/static/img/0D.png"),    
-//     new Card(9, "JACK", "SPADES", "https://deckofcardsapi.com/static/img/JS.png"),
-//     new Card(10, "QUEEN", "SPADES", "https://deckofcardsapi.com/static/img/QS.png"),
-//     new Card(11, "KING", "HEARTS", "https://deckofcardsapi.com/static/img/KH.png"),
-//     new Card(12, "ACE", "DIAMONDS", "https://deckofcardsapi.com/static/img/aceDiamonds.png")
-// ];
 const game = new Game();
 
 game.startGame();
